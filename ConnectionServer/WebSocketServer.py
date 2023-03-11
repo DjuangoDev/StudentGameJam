93355d212b78727fef528baa220485a347a7242d
rafantastico2 <rafantastico+github@gmail.com>
2023-03-11 13:31:47 +0100

    First version of websocket in godot

--- ConnectionServer/WebSocketServer.py
+++ ConnectionServer/WebSocketServer.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python
 
-import asyncio
-import websockets
+# import asyncio
+# import websockets
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+# async def echo(websocket):
+#     async for message in websocket:
+#         await websocket.send(message)
 
-async def main():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+# async def main():
+#     async with websockets.serve(echo, "localhost", 12345):
+#         await asyncio.Future()  # run forever
 
-asyncio.run(main())
+# asyncio.run(main())
+
+from websocket_server import WebsocketServer
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all("Hey all, a new client has joined us")
+
+
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
+
+# Called when a client sends a message
+def message_received(client, server, message):
+	if len(message) > 200:
+		message = message[:200]+'..'
+	print("Client(%d) said: %s" % (client['id'], message))
+
+PORT=12345
+server = WebsocketServer(port = PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
--- ViewerClient/WebSocket/WebSocket.gd
+++ ViewerClient/WebSocket/WebSocket.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # The URL we will connect to
-@export var websocket_url = "wss://localhost:8765"
+@export var websocket_url = "ws://localhost:12345"
 
 # Our WebSocketClient instance
 var _client = WebSocketPeer.new()
@@ -13,27 +13,18 @@ func _ready():
 		print("Unable to connect")
 		set_process(false)
 
-func _closed(was_clean = false):
-	# was_clean will tell you if the disconnection was correctly notified
-	# by the remote peer before closing the socket.
-	print("Closed, clean: ", was_clean)
-	set_process(false)
-
-func _connected(proto = ""):
-	# This is called on connection, "proto" will be the selected WebSocket
-	# sub-protocol (which is optional)
-	print("Connected with protocol: ", proto)
-	# You MUST always use get_peer(1).put_packet to send data to server,
-	# and not put_packet directly when not using the MultiplayerAPI.
-	_client.get_peer(1).send_text("Saludos")
-
-func _on_data():
-	# Print the received packet, you MUST always use get_peer(1).get_packet
-	# to receive data from server, and not get_packet directly when not
-	# using the MultiplayerAPI.
-	print("Got data from server: ", _client.get_peer(1).get_packet().get_string_from_utf8())
-
 func _process(delta):
-	# Call this in _process or _physics_process. Data transfer, and signals
-	# emission will only happen when calling this function.
 	_client.poll()
+	var state = _client.get_ready_state()
+	if state == WebSocketPeer.STATE_OPEN:
+		while _client.get_available_packet_count():
+			print("Packet: ", _client.get_packet().get_string_from_utf8())
+	elif state == WebSocketPeer.STATE_CLOSING:
+		# Keep polling to achieve proper close.
+		pass
+	elif state == WebSocketPeer.STATE_CLOSED:
+		var code = _client.get_close_code()
+		var reason = _client.get_close_reason()
+		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		set_process(false) # Stop processing.
+
